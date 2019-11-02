f190002c442c5ac0040fb72d2ca645a9ba77b776
Chua Hui Shun <huishun@dhcp-206-87-127-32.ubcsecure.wireless.ubc.ca>
2019-11-02 02:45:54 -0700

    README

--- README.md
+++ README.md
@@ -1,24 +1,19 @@
-# portfolio
+# Portfolio
+
+## Description
+A simple portfolio scaffold. The live version can be found here: https://hs-portfolio.herokuapp.com
+
+
+## Language, Frameworks and Libraries
+Built using Vue.js. Works together with Flask backend that helps send emails: https://github.com/huishun98/flask-mailer
 
 ## Project setup
+1. Install dependencies
 ```
 npm install
 ```
-
-### Compiles and hot-reloads for development
+2. Open Vue UI
 ```
-npm run serve
+vue ui
 ```
-
-### Compiles and minifies for production
-```
-npm run build
-```
-
-### Lints and fixes files
-```
-npm run lint
-```
-
-### Customize configuration
-See [Configuration Reference](https://cli.vuejs.org/config/).
+3. Follow instructions in the UI to serve and open app
--- src/components/Projects.vue
+++ src/components/Projects.vue
@@ -7,19 +7,25 @@
           <img class="card-img-top" src="../assets/telebot.png" alt="Card image cap" />
           <div class="card-body">
             <h5 class="card-title">Happy Dinner Bot</h5>
-            <p
-              class="card-text"
-            >Happy Dinner Bot (@happydinnerbot) is a Telegram bot that helps you collate the group members having dinner together tonight.</p>
-            <a
-              href="https://web.telegram.org/#/im?p=@happy_dinner_bot"
-              target="_blank"
-              class="card-link"
-            >Demo</a>
-            <a
-              href="https://github.com/huishun98/happy-dinner-bot-public"
-              target="_blank"
-              class="card-link"
-            >Code</a>
+            <p class="card-text">A Telegram bot helping to organise dinners every day.</p>
+            <p class="card-text">
+              Built using
+              <span class="highlight">Node.js</span> and
+              <span class="highlight">MongoDB</span>. Hosted on
+              <span class="highlight">Heroku</span>.
+            </p>
+            <div>
+              <a
+                href="https://web.telegram.org/#/im?p=@happy_dinner_bot"
+                target="_blank"
+                class="card-link"
+              >Demo</a>
+              <a
+                href="https://github.com/huishun98/happy-dinner-bot-public"
+                target="_blank"
+                class="card-link"
+              >Code</a>
+            </div>
           </div>
         </div>
       </div>
@@ -29,27 +35,69 @@
           <img class="card-img-top" src="../assets/fencing_scoreboard.png" alt="Card image cap" />
           <div class="card-body">
             <h5 class="card-title">Fencing Scoreboard</h5>
-            <p
-              class="card-text"
-            >A scoreboard designed for fencing bouts. Requires two mobile devices.</p>
-            <a
-              href="https://play.google.com/store/apps/details?id=com.scoreboard.scoreboard"
-              target="_blank"
-              class="card-link"
-            >Demo</a>
-            <a href="#" class="card-link btn disabled">Code</a>
+            <p class="card-text">A scoreboard designed for fencing bouts.</p>
+            <p class="card-text">
+              Built using
+              <span class="highlight">React Native</span>.
+            </p>
+            <div>
+              <a
+                href="https://play.google.com/store/apps/details?id=com.scoreboard.scoreboard"
+                target="_blank"
+                class="card-link"
+              >Demo</a>
+              <a href="#" class="card-link btn disabled">Code</a>
+            </div>
           </div>
         </div>
       </div>
 
       <div class="col-md-4">
         <div class="card text-white bg-dark mb-3">
-          <img class="card-img-top" src="../assets/telebot.png" alt="Card image cap" />
+          <img class="card-img-top" src="../assets/portfolio.png" alt="Card image cap" />
           <div class="card-body">
-            <h5 class="card-title">HS Coding Factory</h5>
-            <p class="card-text">A blog.</p>
-            <a href="#" class="card-link">Demo</a>
-            <a href="#" class="card-link btn disabled">Code</a>
+            <h5 class="card-title">HS Portfolio</h5>
+            <p class="card-text">A portfolio website.</p>
+            <p class="card-text">
+              Built using
+              <span class="highlight">Vue.js</span>. Hosted on
+              <span class="highlight">Heroku</span>.
+            </p>
+            <div>
+              <a href="https://hs-portfolio.herokuapp.com/" target="_blank" class="card-link">Demo</a>
+              <a
+                href="https://github.com/huishun98/portfolio"
+                target="_blank"
+                class="card-link"
+              >Code</a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <div class="row section-width cards-wrapper">
+      <div class="col-md-4">
+        <div class="card text-white bg-dark mb-3">
+          <img class="card-img-top" src="../assets/API.png" alt="Card image cap" />
+          <div class="card-body">
+            <h5 class="card-title">Flask Mailer</h5>
+            <p
+              class="card-text"
+            >A simple Flask application that receives POST requests to send emails to specified users.</p>
+            <p class="card-text">
+              Built using
+              <span class="highlight">Flask</span>. Hosted on
+              <span class="highlight">Heroku</span>.
+            </p>
+            <div>
+              <a href="http://hs-flask-mailer.herokuapp.com/" target="_blank" class="card-link">Demo</a>
+              <a
+                href="https://github.com/huishun98/flask-mailer"
+                target="_blank"
+                class="card-link"
+              >Code</a>
+            </div>
           </div>
         </div>
       </div>
